bf5b34bbd005edfcd29831d6a5db80330d0fd48b
dhananjay maurya <dhanuverma9721@gmail.com>
2024-01-27 03:27:36 +0530

    changes done

--- src/Utils/Interfaces.ts
+++
@@ -1,5 +0,0 @@
-export interface IPlayer{
-    name?: string;
-    sessionId?: string;
-    
-}
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,50 @@
 /**
  * IMPORTANT:
  * ---------
- * Do not manually edit this file if you'd like to use Colyseus Arena
+ * Do not manually edit this file if you'd like to host your server on Colyseus Cloud
  *
- * If you're self-hosting (without Arena), you can manually instantiate a
- * Colyseus Server as documented here: 👉 https://docs.colyseus.io/server/api/#constructor-options
+ * If you're self-hosting (without Colyseus Cloud), you can manually
+ * instantiate a Colyseus Server as documented here:
+ *
+ * See: https://docs.colyseus.io/server/api/#constructor-options
  */
-import { listen } from "@colyseus/tools";
+// import { listen } from "@colyseus/tools";
 
-// Import arena config
-import app from "./app.config";
+// // Import Colyseus config
+// import app from "./app.config";
+import { Server } from "colyseus";
+import express from "express";
+import { createServer } from "http";
+import { WebSocketTransport } from "@colyseus/ws-transport";
+import { MyRoom } from "./rooms/MyRoom";
 
-// Create and listen on 2567 (or PORT environment variable.)
-listen(app);
+// // Create and listen on 2567 (or PORT environment variable.)
+// listen(app);
+
+console.info("colyseus server starting");
+// return;
+const portString = process.env.PORT || "2567";
+const port = parseInt(portString, 10);
+const app = express();
+app.use(express.json());
+
+// Use regular HTTP in development
+const server = createServer(app);
+
+let gameServer = new Server({
+    transport: new WebSocketTransport({
+        server,
+        verifyClient: (info: any, next: any) => {
+            next(true);
+        },
+    }),
+});
+
+gameServer.define('GameRoom', MyRoom);
+gameServer.listen(port).then(() => {
+    console.info("Game started and listening on port",port);
+})
+    .catch((err: any) => {
+        console.error("Failed to listen on port" + err)
+    })
+console.info(`Server running on port ${port}`);
